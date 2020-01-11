6ccedebdd21e29d758ad4def2d3b7333ebd99c88
Preacher <jesseh2003@outlook.com>
2020-01-11 01:06:29 -0800

    calculating distance
    you can calculate the estimated distance between the Target by giving the function the angle of the target

---
+++ src/main/java/frc/robot/commands/C_ShieldGeneratorTracking.java
@@ -0,0 +1,76 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import java.lang.Math;
+
+public class C_ShieldGeneratorTracking extends CommandBase {
+  /**
+   * Creates a new ShieldGeneratorTracking.
+   */
+
+float KpAim = -0.1f;
+float KpDistance = -0.1f;
+float minAimCommand = 0.05f;
+float steeringAdjaus = 0.0f;
+double ch = 0; // camera hight
+double th = 0; // target hight
+double angleOftarget = 0;
+double angleofcamera = 0;
+
+  public C_ShieldGeneratorTracking() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  public float GetXPos(float tx, float ty){
+    float headingError = -tx;
+    float distanceError = -ty;
+    float steeringAdjaus = this.steeringAdjaus;
+
+    if (tx > 1.0){
+      steeringAdjaus = KpAim*headingError - minAimCommand;
+    }else if (tx < 1.0){
+      steeringAdjaus = KpAim*headingError + minAimCommand;
+    }
+    float distanceAdjust = KpDistance * distanceError;
+
+    return distanceAdjust + steeringAdjaus;
+  }
+
+  public double EstimateDistance(){
+    double distance = 0;
+
+    distance = (th - ch) / Math.tan((angleofcamera+angleOftarget)*Math.PI/180.0);
+
+    return distance;
+    
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/SS_Vision.java
@@ -0,0 +1,63 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+//Code from limelight example: http://docs.limelightvision.io/en/latest/getting_started.html#basic-programming
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SS_Vision extends SubsystemBase 
+{
+  private NetworkTable table;
+  private NetworkTableEntry tx; //Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
+  private NetworkTableEntry ty; //Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
+  private NetworkTableEntry tv; //Whether the limelight has any valid targets (0 or 1)
+  private NetworkTableEntry ta; //Target Area (0% of image to 100% of image)
+  
+  public SS_Vision() {}
+
+  @Override
+  public void periodic() {}
+
+  public void updateTelemetry()
+  {
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    tx = table.getEntry("tx");
+    ty = table.getEntry("ty");
+    tv = table.getEntry("tv");
+    ta = table.getEntry("ta");
+
+    //read values periodically
+    final double x = tx.getDouble(0.0);
+    final double y = ty.getDouble(0.0);
+    final double area = ta.getDouble(0.0);
+    final boolean target = tv.getDouble(0) == 1;
+
+    //post to smart dashboard periodically
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+    SmartDashboard.putBoolean("Valid Target", target);
+  }
+
+  public double getX()
+  {
+    tx = table.getEntry("tx");
+    return tx.getDouble(0.0);
+  }
+
+  public double getY()
+  {
+    ty = table.getEntry("ty");
+    return tx.getDouble(0.0);
+  }
+}
