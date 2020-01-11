1db2880f404ceb3de6d143ee07ac1215e4627e3d
EricHoelscher <40500077+EricHoelscher@users.noreply.github.com>
2020-01-11 10:33:06 -0800

    Add vision setMode and constants for it

--- src/main/java/frc/robot/Subsystems/SS_Vision.java
+++ src/main/java/frc/robot/Subsystems/SS_Vision.java
@@ -15,26 +15,44 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class SS_Vision extends SubsystemBase 
-{
-  private NetworkTable table;
+public class SS_Vision extends SubsystemBase {
+  //led mode constants
+  public static final int LED_PIPELINE = 0;	//use the LED Mode set in the current pipeline
+  public static final int LED_OFF = 1; //force off
+  public static final int LED_BLINK = 2;	//force blink
+  public static final int LED_ON = 3; //force on
+  public static final int LED_DEFAULT_MODE = LED_ON;
+
+  //camera mode constants
+  public static final int CAMERA_VISION = 0;	//Vision processor
+  public static final int CAMERA_DRIVE = 1; //Driver Camera (Increases exposure, disables vision processing)
+  public static final int CAMERA_DEFAULT_MODE = CAMERA_VISION;
+
+  //piplines (can be a numbers from 0-9)
+  public static final int TRACKING_PIPELINE = 0;
+  public static final int DEFAULT_PIPELINE = TRACKING_PIPELINE;
+
+  //vision network table
+  private NetworkTable visionTable;
   private NetworkTableEntry tx; //Horizontal Offset From Crosshair To Target (-27 degrees to 27 degrees)
   private NetworkTableEntry ty; //Vertical Offset From Crosshair To Target (-20.5 degrees to 20.5 degrees)
   private NetworkTableEntry tv; //Whether the limelight has any valid targets (0 or 1)
   private NetworkTableEntry ta; //Target Area (0% of image to 100% of image)
   
-  public SS_Vision() {}
+  public SS_Vision() {
+    setMode(CAMERA_DEFAULT_MODE, LED_DEFAULT_MODE, DEFAULT_PIPELINE);
+  }
 
   @Override
   public void periodic() {}
 
-  public void updateTelemetry()
-  {
-    table = NetworkTableInstance.getDefault().getTable("limelight");
-    tx = table.getEntry("tx");
-    ty = table.getEntry("ty");
-    tv = table.getEntry("tv");
-    ta = table.getEntry("ta");
+  public void updateTelemetry() {
+
+    visionTable = NetworkTableInstance.getDefault().getTable("limelight");
+    tx = visionTable.getEntry("tx");
+    ty = visionTable.getEntry("ty");
+    tv = visionTable.getEntry("tv");
+    ta = visionTable.getEntry("ta");
 
     //read values periodically
     final double x = tx.getDouble(0.0);
@@ -49,15 +67,25 @@ public class SS_Vision extends SubsystemBase
     SmartDashboard.putBoolean("Valid Target", target);
   }
 
-  public double getX()
-  {
-    tx = table.getEntry("tx");
+  public void setMode(int cameraMode, int ledMode, int pipeline) {  
+    if(cameraMode > -1 && cameraMode < 4) {
+      visionTable.getEntry("camMode").setNumber(cameraMode);
+    }
+    if(ledMode > -1 && ledMode < 2) {
+      visionTable.getEntry("ledMode").setNumber(ledMode);
+    }
+    if(pipeline > -1 && pipeline < 10) {
+      visionTable.getEntry("pipeline").setNumber(pipeline);
+    }
+  }
+
+  public double getX() {
+    tx = visionTable.getEntry("tx");
     return tx.getDouble(0.0);
   }
 
-  public double getY()
-  {
-    ty = table.getEntry("ty");
+  public double getY() {
+    ty = visionTable.getEntry("ty");
     return tx.getDouble(0.0);
   }
 }
